2658f88ac089761f2f9cfcbe49315ae731bbc1e0
Uday kumar <methreudaykumar@gmail.com>
2025-06-09 17:56:58 +0530

    data analytics

--- main.py
+++ main.py
@@ -140,6 +140,15 @@ HTML_FORM = '''
       h2 { font-size: 1.2rem; }
     }
   </style>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XV87H7ZKT8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XV87H7ZKT8');
+</script>
 </head>
 <body>
   <div class="container">
